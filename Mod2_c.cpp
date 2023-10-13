74c3249dd49529fb135b01b26e8642f35d593f18
SabaBegum2 <145177924+SabaBegum2@users.noreply.github.com>
2023-10-13 16:22:58 -0400

    Module3_Q7

--- Mod2_c.cpp
+++ Mod2_c.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
 int main() {
-    float num1, num2, num3, num4;
+    float num1, num2, num3, num4;//the data type for num1, num2, num3, and num4 is float. Float returns decimals. 
+    //cout stands for console out and cin stands for console in.
+    std::cout << "Enter the first number: ";//asking the user to enter the first number,
+    std::cin >> num1;//takes in the first number that is entered by the user. 
+    std::cout << "Enter the second number: ";//askes the user for the second number.
+    std::cin >> num2;//takes in the second number entered by the user.
+    std::cout << "Enter the third number: ";//asks the user for the third number. 
+    std::cin >> num3;//takes in the third numbers entered by the user.
+    std::cout << "Enter the fourth number: ";//askes the user for the fourth numbers.
+    std::cin >> num4;//takes in the forth number that is entered by the user.
 
-    std::cout << "Enter the first number: ";
-    std::cin >> num1;
-    std::cout << "Enter the second number: ";
-    std::cin >> num2;
-    std::cout << "Enter the third number: ";
-    std::cin >> num3;
-    std::cout << "Enter the fourth number: ";
-    std::cin >> num4;
-
-    float average = (num1 + num2 + num3 + num4) / 4;
+    float average = (num1 + num2 + num3 + num4) / 4;//this is the formula for average. It is adding the 4 numbers that is entered by the user.
+    //and then it divides it by 4.
 
     std::cout << "The average of " << num1 << ", " << num2 << ", " << num3 << ", and " << num4 << " is: " << std::fixed << std::setprecision(2) << average << std::endl;
-
+    //this will output the average. 
     return 0;
 }
